34471237b84fa0f487860de0a25ea1e7d896b88d
Michel <michelrostain@laplateforme.io>
2026-02-18 17:11:47 +0100

    Correction code

--- Pokemon/main.py
+++ Pokemon/main.py
@@ -1,12 +1,14 @@
+#Module principal pour lancer le jeu Pokémon.
 import json
 from menu import Menu
 from screen import Interface
 
 def main():
+    # Point d'entrée principal de l'application.Charge les données et initialise l'interface et le menu.
     try:
         with open("pokemon.json", "r", encoding="utf-8") as f:
             data = json.load(f)
-    except:
+    except FileNotFoundError:
         print("Erreur: pokemon.json manquant")
         return
 
